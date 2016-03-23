12dba0071512f3c03f4abc9214338d5e2a81282c
alex <lijie3721@126.com>
2016-03-23 15:41:33 +0800

    堡垒机示例代码

---
+++ sample_code/堡垒机/LittleFinger/bin/little_finger.py
@@ -0,0 +1,11 @@
+#_*_coding:utf-8_*_
+__author__ = 'Alex Li'
+import os,sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.append(BASE_DIR)
+
+if __name__ == '__main__':
+    from modules.actions import excute_from_command_line
+    excute_from_command_line(sys.argv)
